19c19076f585e6e2204acffde110e859d80af1e4
neominal.solutions <neominal.solutions@gmail.com>
2025-10-16 14:03:22 +0300

    JWT Swagger

--- ReactAPISample/Program.cs
+++ ReactAPISample/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ReactAPISample.Services;
 using System.Text;
 
@@ -11,7 +12,40 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(opt =>
+{
+
+  var securityScheme = new OpenApiSecurityScheme()
+  {
+    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+    Name = "Authorization",
+    In = ParameterLocation.Header,
+    Type = SecuritySchemeType.Http,
+    Scheme = "Bearer",
+    BearerFormat = "JWT" // Optional
+  };
+
+  var securityRequirement = new OpenApiSecurityRequirement
+{
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "bearerAuth"
+            }
+        },
+        new string[] {}
+    }
+};
+
+  opt.AddSecurityDefinition("bearerAuth", securityScheme);
+  opt.AddSecurityRequirement(securityRequirement);
+});
+
 
 // web servisler scoped olarak IoC tanýmlandý.
 // project service sýnýfý instance üret. controlerda çaðýrýcaz.
